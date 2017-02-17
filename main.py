e9f3ce993c71ae2c48cd32518039835b278ec531
Mika Koivusaari <mika.koivusaari@gmail.com>
2017-02-17 20:42:52 +0200

    Updating time from ntp.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from machine import I2C, Pin
 from servo import Servo
 import onewire, ds18x20
 from esp8266_i2c_lcd import I2cLcd
+import ubinascii
+from umqtt.simple import MQTTClient
+import ntptime
 
 servo_min_angle = 10
 servo_max_angle = 160
@@ -11,6 +14,13 @@ servo_angle=servo_min_angle
 servo_adjust_angle=10
 # the device is on GPIO12
 dat = machine.Pin(14)
+
+#sleep time in every loop
+sleep_time=10000
+#send values every Nth loop
+send_values=6 #every minute
+#update time from ntp every Nth loop
+update_time=6*60 #every hour
 
 # create the onewire object
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
@@ -51,22 +61,23 @@ for rom in roms:
 #print('found devices:', roms)
 
 # loop 10 times and print all temperatures
-for i in range(10):
+update_time_i=update_time
+while True:
+    if update_time_i==0:
+        ntp.settime()
+        update_time_i=update_time
+    update_time_i=update_time_i-1
+    
     ds.convert_temp()
     time.sleep_ms(1000)
 
     inside_temp = ds.read_temp(inside_rom)
     outside_temp = ds.read_temp(outside_rom)
     heated_temp = ds.read_temp(heated_rom)
-    
-    lcd.clear()
-    lcd.putstr("In % 3.0f Out % 3.0f\nHeated % 3.0f %d" %
-               (inside_temp, outside_temp, heated_temp, servo_angle))
-    print(".")
 
     if heated_temp>inside_temp+2:
         servo_angle=servo_angle+servo_adjust_angle
-    if heated_temp<inside_temp+1:
+    if heated_temp<inside_temp:
         servo_angle=servo_angle-servo_adjust_angle
 
     if servo_angle>servo_max_angle:
@@ -75,6 +86,11 @@ for i in range(10):
         servo_angle=servo_min_angle
 
     servo.write_angle(degrees=servo_angle)
+
+    lcd_str = "In % 3.0f Out % 3.0f\nHeated % 3.0f   %d" %(inside_temp, outside_temp, heated_temp, servo_angle)
+    lcd.clear()
+    lcd.putstr(lcd_str)
+    print(lcd_str)
 
     time.sleep_ms(10000)
 
